c085873da0d20976511b38babfc810a616ae2294
Sabinu <sabin.purice@gmaill.com>
2013-11-10 00:43:40 +0200

    Quiz - Done

---
+++ _Quiz/p_04.py
@@ -0,0 +1,17 @@
+__author__ = 'Sabinu'
+
+
+def myLog(x, b):
+    """
+    x       : a positive integer
+    b       : a positive integer; b >= 2
+    returns : log_b(x), or, the logarithm of x relative to a base b.
+    """
+    if x < b:
+        return 0
+    else:
+        return 1 + myLog(x/b, b)
+
+print myLog(16, 2)
+print myLog(15, 3)
+print myLog(234, 2)
---
+++ _Quiz/p_06.py
@@ -0,0 +1,26 @@
+__author__ = 'Sabinu'
+
+
+def laceStringsRecur(s1, s2):
+    """
+    @param s1 : string
+    @param s2 : string
+    @return   : str with elements of s1 and s2 interlaced,
+                beginning with s1. If strings are not of same length,
+                then the extra elements should appear at the end.
+    """
+    def helpLaceStrings(s1, s2, out):
+        if s1 == '':
+            return out + s2
+        if s2 == '':
+            return out + s1
+        else:
+            return helpLaceStrings(s1[1:], s2[1:], out + s1[0] + s2[0])
+    return helpLaceStrings(s1, s2, '')
+
+## TEST SUITE
+print laceStringsRecur('abcd',   '12345'), '\n____________'
+print laceStringsRecur('123456', 'abcd'),  '\n____________'
+print laceStringsRecur('abcd',   ''),      '\n____________'
+print laceStringsRecur('',       '12345'), '\n____________'
+print laceStringsRecur('',       ''),      '\n____________'
