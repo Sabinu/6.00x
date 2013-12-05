c608c4eb9eb2074eb423831395e1f8b36e9d9245
Sabin Purice <sabin.purice@gmail.com>
2013-12-05 17:55:05 +0200

    Plotting - Work in Progress

---
+++ Lecture 14 - Plotting/Lec_14__p_1.py
@@ -0,0 +1,17 @@
+__author__ = 'Sabinu'
+
+
+def isAlphabeticalWord(word, wordList=None):
+    if len(word) > 0:
+        curr = word[0]
+    for letter in word:
+        if curr > letter:
+            return False
+        else:
+            curr = letter
+    if wordList is None:
+        return True
+    return word in wordList
+
+print isAlphabeticalWord('abcd')
+print isAlphabeticalWord('zoo')
