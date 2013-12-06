d532e373155b614cfe58971069f6545419a45fb1
Sabin Purice <sabin.purice@gmail.com>
2013-12-06 11:49:49 +0200

    syntax check

--- Lecture 14 - Plotting/plotting-seg3.py
+++ Lecture 14 - Plotting/plotting-seg3.py
@@ -13,17 +13,19 @@ pylab.rcParams['ytick.major.size'] = 5
 
 
 def findPayment(loan, r, m):
-    """Assumes: loan and r are floats, m an int
+    """
+    Assumes: loan and r are floats, m an int
     Returns the monthly payment for a mortgage of size
-    loan at a monthly rate of r for m months"""
-    return loan*((r*(1+r)**m)/((1+r)**m - 1))
+    loan at a monthly rate of r for m months
+    """
+    return loan * ((r * (1 + r) ** m) / ((1 + r) ** m - 1))
 
 
 class MortgagePlots(object):
-    
+
     def plotPayments(self, style):
         pylab.plot(self.paid[1:], style, label=self.legend)
-        
+
     def plotTotPd(self, style):
         totPd = [self.paid[0]]
         for i in range(1, len(self.paid)):
@@ -36,7 +38,7 @@ class Mortgage(MortgagePlots, object):
     def __init__(self, loan, annRate, months):
         """Create a new mortgage"""
         self.loan = loan
-        self.rate = annRate/12.0
+        self.rate = annRate / 12.0
         self.months = months
         self.paid = [0.0]
         self.owed = [loan]
@@ -46,7 +48,7 @@ class Mortgage(MortgagePlots, object):
     def makePayment(self):
         """Make a payment"""
         self.paid.append(self.payment)
-        reduction = self.payment - self.owed[-1]*self.rate
+        reduction = self.payment - self.owed[-1] * self.rate
         self.owed.append(self.owed[-1] - reduction)
 
     def getTotalPaid(self):
@@ -61,7 +63,7 @@ class Mortgage(MortgagePlots, object):
 class Fixed(Mortgage):
     def __init__(self, loan, r, months):
         Mortgage.__init__(self, loan, r, months)
-        self.legend = 'Fixed, ' + str(r*100) + '%'
+        self.legend = 'Fixed, ' + str(r * 100) + '%'
 
 
 # fixed-rate mortgage with up-front points
@@ -69,7 +71,7 @@ class FixedWithPts(Fixed):
     def __init__(self, loan, r, months, pts):
         Fixed.__init__(self, loan, r, months)
         self.pts = pts
-        self.paid = [loan*(pts/100.0)]
+        self.paid = [loan * (pts / 100.0)]
         self.legend += ', ' + str(pts) + ' points'
 
 
@@ -79,10 +81,10 @@ class TwoRate(Mortgage):
         Mortgage.__init__(self, loan, teaserRate, months)
         self.teaserMonths = teaserMonths
         self.teaserRate = teaserRate
-        self.nextRate = r/12.0
-        self.legend = str(teaserRate*100)\
+        self.nextRate = r / 12.0
+        self.legend = str(teaserRate * 100)\
             + '% for ' + str(self.teaserMonths)\
-            + ' months, then ' + str(r*100) + '%'
+            + ' months, then ' + str(r * 100) + '%'
 
     def makePayment(self):
         if len(self.paid) == self.teaserMonths + 1:
@@ -93,10 +95,10 @@ class TwoRate(Mortgage):
 
 
 class MortgagePlots(object):
-    
+
     def plotPayments(self, style):
         pylab.plot(self.paid[1:], style, label=self.legend)
-        
+
     def plotTotPd(self, style):
         totPd = [self.paid[0]]
         for i in range(1, len(self.paid)):
@@ -130,7 +132,7 @@ def plotMortgages(morts, amt):
 
 def compareMortgages(amt, years, fixedRate, pts, ptsRate,
                      varRate1, varRate2, varMonths):
-    totMonths = years*12
+    totMonths = years * 12
     fixed1 = Fixed(amt, fixedRate, totMonths)
     fixed2 = FixedWithPts(amt, ptsRate, totMonths, pts)
     twoRate = TwoRate(amt, varRate2, totMonths,
@@ -142,8 +144,13 @@ def compareMortgages(amt, years, fixedRate, pts, ptsRate,
     plotMortgages(morts, amt)
 
 
-compareMortgages(amt=200000, years=30, fixedRate=0.07,
-                 pts=3.25, ptsRate=0.05, varRate1=0.045,
-                 varRate2=0.095, varMonths=48)
+compareMortgages(amt        =200000,
+                 years      =    30,
+                 fixedRate  =  0.07,
+                 pts        =  3.25,
+                 ptsRate    =  0.05,
+                 varRate1   = 0.045,
+                 varRate2   = 0.095,
+                 varMonths  =    48)
 
 pylab.show()
